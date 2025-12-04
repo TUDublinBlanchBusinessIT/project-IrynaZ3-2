c816dfc39d3c52678430ab0ee674ade651bc18ea
B00165879 Iryna Zabolotna <B00165879@mytudublin.ie>
2025-12-04 11:14:53 +0000

    chnages

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Insert Data</title>
+</head>
+<body>
+<h1>Add a Person</h1>
+
+<form action="insert.php" method="POST">
+    Name: <input type="text" name="name" required><br><br>
+    Email: <input type="email" name="email" required><br><br>
+    <button type="submit">Save</button>
+</form>
+
+<br><br>
+<a href="view.php">View All Records</a>
+
+</body>
+</html>
